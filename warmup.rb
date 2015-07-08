f336a23911170d7de93a38e266ec1b8cc7a81ef3
Alice Fung <alice.o.fung@gmail.com>
2015-07-08 14:56:28 -0400

    add print_matrix method

--- warmup.rb
+++ warmup.rb
@@ -1,7 +1,7 @@
 require_relative 'edge_list'
 
 class AdjacencyMatrix
-  attr_reader :edge_list, :id
+  attr_reader :edge_list, :id, :matrix
 
   def initialize(edge_list)
     @edge_list = edge_list
@@ -9,35 +9,45 @@ class AdjacencyMatrix
   end
 
 
-  # def print_matrix
-  #  @edge_list.list.each do |person|
-  #    puts person[0][1]
-  # end
+  def print_matrix
+    (0..@vertices-1).each do |id|
+      print id
+      print "\t"
+    end
+    print "\n"
+
+   @matrix.each do |row|
+    row.each do |item|
+      item.nil? ? (print "X") : (print item)
+      print "\t"
+    end
+    print "\n"
+   end
+  end
 
   def build_matrix
-    @id = Array.new() { Array.new() { } }
+    @matrix = Array.new(@vertices)
+    (0..@vertices-1).each do |index|
+      @matrix[index] = Array.new(@vertices)
+    end
    # id[0] = [4, 8 ,8 7]
     @edge_list.list.each do |edge|
       # @edge_list.list.length.times do |i|
         # @arr[edge[0].id] << edge[2]
-      @id[edge[0].id][edge[1].id]<< edge[2]
+      @matrix[edge[0].id][edge[1].id] = edge[2]
     end
-
-
   end
 
 
-  # def collect_id
-  #   @edge_list.list.each do |edge|
-  #     id1= edge[0].id
-  #     puts "#{id1} + 0,0"
-  #     id2= edge[1].id
-  #     puts "#{id2} + 0,1"
-  #     @id << id1 unless @id.include?(id1)
-  #     @id << id2 unless @id.include?(id2)
-  #   end
-  #   p @id
-  # end
+  def num_vertex
+    @edge_list.list.each do |connection|
+      id1 = connection[0].id
+      id2 = connection[1].id
+      @id << id1 unless @id.include?(id1)
+      @id << id2 unless @id.include?(id2)
+    end
+    @vertices = @id.length
+  end
 
 
 end
